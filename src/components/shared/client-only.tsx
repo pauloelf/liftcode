3482d2b388efdaacae139c0766106209fac2fb06
Paulo Sergio <contato.paulofws@gmail.com>
2025-12-01 19:30:10 -0300

    feat: add client-only component

---
+++ src/components/shared/client-only.tsx
@@ -0,0 +1,12 @@
+"use client"
+import type * as React from "react"
+import { useEffect, useState } from "react"
+
+export function ClientOnly({ children }: { children: React.ReactNode }) {
+  const [mounted, setMounted] = useState(false)
+
+  useEffect(() => setMounted(true), [])
+
+  if (!mounted) return null
+  return <>{children}</>
+}
