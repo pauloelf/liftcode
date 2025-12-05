c006b6581e497c832da9d26f16a82d03d3ef5c2f
Paulo Sergio <contato.paulofws@gmail.com>
2025-12-05 18:43:44 -0300

    feat: add getPosts function

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -11,3 +11,13 @@ export async function getProjects() {
   )
   return (await response.json()).data
 }
+
+export async function getPosts() {
+  const API_URL = process.env.NEXT_PUBLIC_API_URL
+  cacheLife("minutes")
+
+  const response = await fetch(
+    `${API_URL}/posts?sort=createdAt:desc&populate=*`,
+  )
+  return (await response.json()).data
+}
