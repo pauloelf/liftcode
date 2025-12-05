f78b7eb82a18bda665246146f1d291abaea669b6
Paulo Sergio <contato.paulofws@gmail.com>
2025-12-04 22:55:07 -0300

    feat: add getProjects function

---
+++ src/lib/api.ts
@@ -0,0 +1,13 @@
+"use cache"
+
+import { cacheLife } from "next/cache"
+
+export async function getProjects() {
+  const API_URL = process.env.NEXT_PUBLIC_API_URL
+  cacheLife("minutes")
+
+  const response = await fetch(
+    `${API_URL}/projects?sort=createdAt:desc&populate=*`,
+  )
+  return (await response.json()).data
+}
