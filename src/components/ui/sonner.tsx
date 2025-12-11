54b6bb133e4a6879c9fee779b706c8d7f82d0b26
Paulo Sergio <contato.paulofws@gmail.com>
2025-12-10 21:53:23 -0300

    refactor: update colors

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -26,11 +26,11 @@ const Toaster = ({ ...props }: ToasterProps) => {
     <Sonner
       className="group toaster"
       icons={{
-        success: <CircleCheckIcon className="size-4" />,
+        success: <CircleCheckIcon className="size-4 text-green-500" />,
         info: <InfoIcon className="size-4" />,
-        warning: <TriangleAlertIcon className="size-4" />,
-        error: <OctagonXIcon className="size-4" />,
-        loading: <Loader2Icon className="size-4 animate-spin" />,
+        warning: <TriangleAlertIcon className="size-4 text-yellow-500" />,
+        error: <OctagonXIcon className="size-4 text-destructive" />,
+        loading: <Loader2Icon className="size-4 text-accent animate-spin" />,
       }}
       style={
         {
