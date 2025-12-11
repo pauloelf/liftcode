3c88a737d32f608ec4c31079692357fab5b4ec97
Paulo Sergio <contato.paulofws@gmail.com>
2025-12-10 22:00:23 -0300

    feat: add home page

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,22 @@
+import { getPosts, getProjects } from "@/lib/api"
+import { ContactSection } from "./_components/contact-section"
+import { FeaturedProjects } from "./_components/featured-projects"
+import { HeroSection } from "./_components/hero-section"
+import { LatestPosts } from "./_components/latest-posts"
+import { SkillsSection } from "./_components/skills-section"
+import { TechSection } from "./_components/tech-section"
+
+export default async function HomePage() {
+  const projects = await getProjects()
+  const posts = await getPosts()
+  return (
+    <main className="flex flex-col justify-center">
+      <HeroSection />
+      <FeaturedProjects projects={projects} />
+      <LatestPosts posts={posts} />
+      <TechSection />
+      <SkillsSection />
+      <ContactSection />
+    </main>
+  )
+}
