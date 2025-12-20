5a6e87eeee0877c01cbf2a600efd183d7deb296a
Paulo Sergio <contato.paulofws@gmail.com>
2025-12-20 16:41:43 -0300

    fix: Changes id types from number to string

--- src/@types/post-types.ts
+++ src/@types/post-types.ts
@@ -1,5 +1,5 @@
 export type Post = {
-  id: number
+  id: string
   title: string
   description: string
   createdAt: string
--- src/@types/project-types.ts
+++ src/@types/project-types.ts
@@ -1,7 +1,7 @@
 import type * as React from "react"
 
 export type Project = {
-  id: number
+  id: string
   title: string
   description: string
   tags: string[]
